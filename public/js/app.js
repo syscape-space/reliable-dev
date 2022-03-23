ece75760a7022a1ae3252200b887a3afb8af51fd
Mostafa Gamal <52170011+mostafa42@users.noreply.github.com>
2022-03-23 05:16:46 -0700

    editting3 commit

--- public/js/app.js
+++ public/js/app.js
@@ -37552,7 +37552,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
 
-var env = "local"; // can be local or production
+var env = "production"; // can be local or production
 
 var api = axios__WEBPACK_IMPORTED_MODULE_0___default().create({
   baseURL: env == "local" ? "http://localhost/reliable/api" : 'https://law-mawthuq.com/reliable/api',
