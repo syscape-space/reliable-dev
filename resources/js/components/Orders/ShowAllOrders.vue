afdbd793c42b142c4af3571b9103aadf386e1be3
mohamedhendy <mohamadhendy14@gmail.com>
2022-04-10 16:13:21 +0200

    dropdown btns

--- resources/js/components/Orders/ShowAllOrders.vue
+++ resources/js/components/Orders/ShowAllOrders.vue
@@ -3,9 +3,7 @@
     <div class="personal">
       <div class="personal-info">
         <div class="btw-flex">
-          <div class="position-relative">
-            
-          </div>
+          <div class="position-relative"></div>
           <div>
             <div class="btn-group">
               <button
@@ -31,17 +29,96 @@
           </div>
         </div>
 
-        <div>
-          <button class="btn btn-success px-3">اختيار القسم</button>
-          <button class="btn btn-warning px-3 mx-1">الفرعى</button>
-          <button class="btn btn-info px-3 mx-1">اختيار المدينة</button>
-          <button class="btn btn-primary px-3">الكل</button>
-          <button class="btn btn-danger px-3 mx-1">عرض النتائج</button>
+        <div class="d-flex align-items-center">
+          <div class="dropdown mx-1">
+            <button
+              class="btn btn-success dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              اختيار القسم
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+          <div class="dropdown mx-1">
+            <button
+              class="btn btn-warning dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              الفرعى
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+          <div class="dropdown mx-1">
+            <button
+              class="btn btn-info dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              اختيار المدينة
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+          <div class="dropdown mx-1">
+            <button
+              class="btn btn-primary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              الكل
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+          <div class="dropdown mx-1">
+            <button
+              class="btn btn-danger  dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              عرض النتائج
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
         </div>
-        <div class="p-3 mt-3" style="background-color: #f9f9f9" v-for="item in list2" :key="item.id">
-          <order-card :order="item"/>
+        <div
+          class="p-3 mt-3"
+          style="background-color: #f9f9f9"
+          v-for="item in list2"
+          :key="item.id"
+        >
+          <order-card :order="item" />
         </div>
-
       </div>
     </div>
   </section>
@@ -50,33 +127,31 @@
 import api from "../../utils/api";
 import OrderCard from "./OrderCard";
 export default {
-  components: {OrderCard},
+  components: { OrderCard },
   mounted() {
-     // let thisorderId = localStorage.getItem("thisOrderId");
-    // check user type 
+    // let thisorderId = localStorage.getItem("thisOrderId");
+    // check user type
     api
-        .get("/v1/account")
-        .then((response) => {
-          if( response.data.data.membership_type === 'vendor' ){ // مقدم خدمه
-              this.getMyOrders2();
-          }else if( response.data.data.membership_type === 'user' ){
-            this.$router.push({ name: "MyOrder" });
-          }
-        })
-        // error.response.data.errors
-        .catch((e) => {
-          this.errors = e.response.data.errors;
-          console.log(e.response);
-        });
-  
-
-   
+      .get("/v1/account")
+      .then((response) => {
+        if (response.data.data.membership_type === "vendor") {
+          // مقدم خدمه
+          this.getMyOrders2();
+        } else if (response.data.data.membership_type === "user") {
+          this.$router.push({ name: "MyOrder" });
+        }
+      })
+      // error.response.data.errors
+      .catch((e) => {
+        this.errors = e.response.data.errors;
+        console.log(e.response);
+      });
   },
   data() {
     return {
       base_url: base_url,
-      cloud_url:cloud_url,
-      list2 : [] ,
+      cloud_url: cloud_url,
+      list2: [],
     };
   },
   methods: {
@@ -85,7 +160,6 @@ export default {
         .get("v1/orders")
         .then((response) => {
           this.list2 = response.data.data.data;
-          
         })
         .catch((e) => {
           console.log(e.response);
@@ -94,3 +168,10 @@ export default {
   },
 };
 </script>
+
+<style>
+button:not(.collapsed)::after {
+    background-image: url(/images/down-aa.svg?18e7856…);
+    transform: rotate(0deg);
+}
+</style>
