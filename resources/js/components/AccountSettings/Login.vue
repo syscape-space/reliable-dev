9c17e2e51eec79044c047b437201cca90c3752b2
unknown <himasami0000@gmail.com>
2022-04-16 03:20:22 +0200

    edit login page

--- resources/js/components/AccountSettings/Login.vue
+++ resources/js/components/AccountSettings/Login.vue
@@ -1,27 +1,27 @@
-<template>  
-    <div class="container">
-      <Navbar/>
-    </div>
-    <div class="row w-100 mx-0 px-0">
+<template >  
+  
+    <div class="container" >
+      
+    <div class="row w-100 mx-0 px-0 mt-5" >
+        <div class="col-md-6 login-page" id="pageLoginImage">
+          <div class="text-center mt-5">
+            <!--<img style="width: 150px;" :src="base_url+'/assets/images/login-1-logo.svg'" alt="">-->
+          </div>
+        </div>
         <div class="col-md-6 px-0 ">
-          <div class="p-5">
-            <div class="text-center mt-5">
-              <img style="width: 100px;" :src="base_url+'/public/assets/images/login-1-logo.svg'" alt="">
+          <div class="p-3">
+            <div class="text-center mt-1 mb-5 logoText">
+              <!--<img style="width: 100px;" :src="base_url+'/public/assets/images/login-1-logo.svg'" alt="">-->
+              <img style="width: 30%;object-fit:contain" src="../../assets/images/logoText.png" alt="">
             </div>
-            <div class="register text-center  ">
-              <p class="my-4 "> {{ $root._t("app.do_you_have_account") }}
-                <router-link :to="{name:'Register'}"
-                  href="#"
-                  style="color: #048e81;"  
-                class=""> {{ $root._t("app.create_account_now") }} </router-link></p>
-              <h3 class="mb-4">  {{ $root._t("app.login") }} </h3>
-            
+            <div class="register text-center" style="color:#0995EB">
+              <h4 class="mb-4">  {{ $root._t("app.login") }} </h4>
             </div>
             <form action="" class="login-form text-center" style="max-width: 400px;margin: auto;">
-              <small class="d-inline-block mb-4 text-end mx-auto" 
+              <!--<small class="d-inline-block mb-4 text-end mx-auto" 
                 style="max-width: 300px; color: #0995eb;">
                 {{ $root._t("app.folow_instructions") }}
-              </small>
+              </small>-->
               <!--- Error Will Validate Here -->
               <div class="errors">
                 <div class="alert alert-danger" v-for="error in errors" :key="error">
@@ -29,28 +29,46 @@
                 </div>
               </div>
               <div class="auth-login mb-4 text-center  position-relative">
-                <input type="number" class="pe-5 form-control" placeholder="   رقم التحقق" v-model="id_number"> <!-- Phone number -->
+                <label class="w-100 d-flex align-items-center mb-2 text-right" style="column-gap:5px;color:#4E5567">
+                  <i class="fas fa-phone-alt"></i>
+                  <span>رقم الجوال</span>
+                </label>
+                <input type="number" class="pe-2 form-control" placeholder=" رقم الجوال" v-model="id_number"> <!-- Phone number -->
                 <img class="img-lllo" style="width: 20px; position: absolute; right: 10px;"
                 :src="base_url+'/assets/images/login-user.svg'" alt="">
               </div>
               <div class="auth-login text-center  position-relative">
-                <input type="password" class="pe-5 form-control" placeholder="كلمة المرور " v-model="password"> <!-- Password -->
+                <label class="w-100 d-flex align-items-center mb-2 text-right" style="column-gap:5px;color:#4E5567">
+                  <i class="fas fa-lock"></i>
+                  <span>كلمة المرور</span>
+                </label>
+                <input type="password" class="pe-2 form-control" placeholder="كلمة المرور " v-model="password"> <!-- Password -->
                 <img class="img-lllo" style="width: 12px; position: absolute; right: 10px;"
                 :src="base_url+'/assets/images/login-pass.svg'" alt="">
               </div>
               <div class="mt-3 d-flex align-items-center justify-content-between">
-                <div>
+                <!--<div>
                   <label for=""> {{ $root._t("app.do_you_forget_password") }}  </label>
-                </div>
+                </div>-->
                 <div class="">
+                  <input type="checkbox" style="vertical-align: middle;margin-left:10px">
                   <label for=""> {{ $root._t("app.remember_me") }} </label>
-                  <input type="checkbox" style="vertical-align: middle;">
+                </div>
+                <div style="display:none">
+                  <span style="color:red">اعادة ارسال في <span> 25 </span>ثانية</span>
                 </div>
               </div>
               <div>
                 <button class="login-btn" @click.prevent="loginFun()"> {{ $root._t("app.login_now") }} </button>
               </div>
-              <div class="social-btn d-flex align-items-center justify-content-between" >
+              <div>
+                <p class="my-4 "> {{ $root._t("app.do_you_have_account") }}
+                    <router-link :to="{name:'Register'}"
+                      href="#"
+                      style="color: #048e81;"  
+                    class=""> {{ $root._t("app.create_account_now") }} </router-link></p> 
+              </div>
+                <!--  <div class="social-btn d-flex align-items-center justify-content-between" >
                 <button>
                   {{ $root._t("app.facebook") }} 
                   <img 
@@ -61,16 +79,14 @@
                   {{ $root._t("app.google") }} 
                   <img style="width: 19px;" :src="base_url+'/assets/images/google-llc.png'" alt="">
                 </button>
-              </div>
+              </div>-->
             </form>
           </div>
         </div>
-        <div class="col-md-6 login-page" id="pageLoginImage">
-          <div class="text-center mt-5">
-            <img style="width: 150px;" :src="base_url+'/assets/images/login-1-logo.svg'" alt="">
-          </div>
-        </div>
+        
       </div>
+    </div>
+
 </template>
 
 <script>
