1f55aae187cd6c16f92e43f0f259da134dd668dc
mohamedhendy <mohamadhendy14@gmail.com>
2022-05-07 13:16:16 +0200

    fonts problem

--- resources/views/front/layout/head.blade.php
+++ resources/views/front/layout/head.blade.php
@@ -6,20 +6,38 @@
 <link rel="stylesheet" href="{{asset('assets/css/all.min.css')}}" />
 <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
 <link href='http://fonts.googleapis.com/earlyaccess/droidarabickufi.css' rel='stylesheet' type='text/css' />
-
-
-<link rel="stylesheet" href="{{asset('assets/css/style.css')}}" />
-<link rel="stylesheet" href="{{asset('tem_assets/css/style.css')}}" />
 <link rel="stylesheet" href="{{asset('tem_assets')}}/css/normalize.css" />
-<!-- Bootstrap -->
+
 <link rel="stylesheet" href="{{asset('tem_assets')}}/css/bootstrap.min.css" />
 <!-- Font Awesome -->
 <link rel="stylesheet" href="{{asset('tem_assets')}}/css/all.min.css" />
+<link rel="stylesheet" href="{{asset('assets/css/style.css')}}" />
+<link rel="stylesheet" href="{{asset('tem_assets/css/style.css')}}" />
+<!-- Bootstrap -->
+
 <!-- Main Faile Css  -->
-@stack('page_styles')
-
-@livewireStyles
 <style>
-
+    @font-face {
+  font-family: "din-next";
+  src: url("../fonts/din-next-lt-w23-light-1.ttf");
+  font-weight: 300;
+}
+.f-300 {
+  font-weight: 300;
+}
+@font-face {
+  font-family: "din-next";
+  src: url("../fonts/din-next-lt-w23-regular.ttf");
+  font-weight: normal;
+}
+@font-face {
+  font-family: "din-next";
+  src: url("../fonts/din-next-lt-w23-bold-1.ttf");
+  font-weight: bold;
+}
 </style>
+@stack('page_styles')
+    
+@livewireStyles
+
 <title>@yield('title')</title>
